bb2da688c763f527e241f7d08b7abb4242733dbf
santinopacheco <sanpachiluna@gmail.com>
2024-06-11 09:19:25 -0300

    cambio en el nav

---
+++ code.js
@@ -0,0 +1,12 @@
+var header = document.getElementById('Header')
+
+window.addEventListener("scroll", function(){
+    var scroll = window.scrollY;
+    if(scrollY>0){
+        header.style.backgroundColor = '#121212';
+    }
+    else{
+        header.style.backgroundColor = 'transparent';
+    }
+
+})
